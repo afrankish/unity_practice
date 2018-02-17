fea9c78e1201efa2cf07a6085cb4ef9612c5a158
William Frankish <wfrankish@gmail.com>
2018-02-17 14:54:08 +0000

    allow basic movement

--- Marble Game/Assets/Scripts/PlayerController.cs
+++ Marble Game/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rb;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float Speed = 10;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        _rb.AddForce(movement * Speed);
+    }
 }
