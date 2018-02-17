ece69a543bce506c5668d3904b6175ab88a94544
William Frankish <wfrankish@gmail.com>
2018-02-17 15:36:02 +0000

    enable horizontal camera rotation

--- Marble Game/Assets/Scripts/CameraController.cs
+++ Marble Game/Assets/Scripts/CameraController.cs
@@ -4,6 +4,7 @@ public class CameraController : MonoBehaviour
 {
     private Vector3 _offset;
 
+    public float RotationalSpeed = 90;
     public GameObject Focus;
 
     private void Start()
@@ -11,8 +12,17 @@ public class CameraController : MonoBehaviour
         _offset = transform.position - Focus.transform.position;
     }
 
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("CamHorizontal");
+        float angle = Time.deltaTime * RotationalSpeed;
+
+        _offset = Quaternion.AngleAxis(angle, transform.up) * _offset;
+    }
+
     private void LateUpdate()
     {
         transform.position = Focus.transform.position + _offset;
+        transform.LookAt(Focus.transform);
     }
 }
